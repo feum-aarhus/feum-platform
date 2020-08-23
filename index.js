83f243a3df4af2d34ff5e314ef80f3e0514fa5ce
Vít Brykner <johnny.brykner@gmail.com>
2020-08-23 23:09:30 +0200

    Merge script files

--- index.js
+++ index.js
@@ -24,4 +24,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const response = await rawData.json();
     console.log(response);
   }
+
+  const processHeader = document.querySelector(".event_process-header");
+  const steps = document.querySelector(".event_the-steps");
+  processHeader.addEventListener("click", () => {
+    steps.classList.remove("event_process-hidden");
+    steps.classList.add("event_process-displaying");
+  });
 });
--- stuff.js
+++
@@ -1,6 +0,0 @@
-const processHeader = document.querySelector(".event_process-header");
-const steps = document.querySelector(".event_the-steps");
-processHeader.addEventListener("click", () => {
-  steps.classList.remove("event_process-hidden");
-  steps.classList.add("event_process-displaying");
-});
